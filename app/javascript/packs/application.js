6018c011ad182b4faa8df798a69344119414edfc
uuutako <okn.sk009@gmail.com>
2020-12-14 21:15:01 +0900

    Revert "gem 環境変数(twitter)の削除"
    This reverts commit 8af04a86827b43cb1240004ec7c0ed21f40a5643.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,13 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+//= require jquery
+//= require jquery_ujs
 
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 
 
 
